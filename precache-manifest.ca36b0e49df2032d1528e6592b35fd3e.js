ed5d307550be25cc80046ac59cd8ddd5e66a2601
Geoff <gsmit592@ford.com>
2020-03-23 14:25:22 +0000

    Updates

---
+++ precache-manifest.ca36b0e49df2032d1528e6592b35fd3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8964720d20a2d2220e12d2e7335cd02",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "62311d55c97ccf384301",
+    "url": "/monsters-rolodex/static/css/main.ebc51286.chunk.css"
+  },
+  {
+    "revision": "eb8c292b02fc765d0844",
+    "url": "/monsters-rolodex/static/js/2.dca1f432.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.dca1f432.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62311d55c97ccf384301",
+    "url": "/monsters-rolodex/static/js/main.007a0987.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
